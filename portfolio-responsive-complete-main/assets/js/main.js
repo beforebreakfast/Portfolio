332e18cc0dd2a38a7ad0494fb18e2c1d0b5bef54
Thamonwan <gamthamonwan04@gmail.com>
2026-02-03 00:47:48 +0700

    Initial commit

---
+++ portfolio-responsive-complete-main/assets/js/main.js
@@ -0,0 +1,58 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+    if (!sectionsClass) return;
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 200,
+});
+
+// ปรับ selector ให้เข้ากับ section ใหม่
+sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text, .section-title", {});
+sr.reveal(".home__img, .about__subtitle, .about__text", { delay: 350 });
+sr.reveal(".home__social-icon", { interval: 150 });
+sr.reveal(".education__item, .skills__group, .work__card, .contact__input", { interval: 150 });
